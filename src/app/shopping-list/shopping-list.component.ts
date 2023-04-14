b071455f095a4ae5196039d8aadfa7f788807118
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2023-04-13 23:07:42 -0300

    keep moving foward with the cooking recipes project.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Garlic', 2),
+    new Ingredient('Lettuce', 1)
+  ];
 
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 }

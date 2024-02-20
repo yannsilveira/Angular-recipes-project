8f50a79adb8d2dad8312d736b41dc31cb0f68af5
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2024-02-19 23:44:28 -0300

    continuing with the recipes Angular project.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('Banana Split', 'A delicious Banana Split', 'https://www.comidaereceitas.com.br/wp-content/uploads/2008/05/banana_split.jpg'),
     new Recipe('Feijoada', 'Uma deliciosa feijoada para o Domingo', 'https://blog.biglar.com.br/wp-content/uploads/2021/10/typical-brazilian-dish-called-feijoada-made-with-black-beans-pork-sausage.jpg'),
@@ -16,6 +18,10 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
     
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
+  selectedRecipe: Recipe;
+  
+  constructor() {
 
+  }
+
+  ngOnInit(): void {
+    
+  }
 }

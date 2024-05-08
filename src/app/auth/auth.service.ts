a97c8ac4edd8b880ab8de20a92de6e23763f6b9c
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2024-05-08 17:43:30 -0300

    Handling errors when logging in and registering a new account.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string;
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
@@ -16,11 +19,44 @@ export class AuthService {
  
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB7lRrlOBoxtJiSD-kq2CX6t7F3aCdS__8',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        )
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }).pipe(catchError(this.handleError));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7lRrlOBoxtJiSD-kq2CX6t7F3aCdS__8', 
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch(errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists!';
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = 'Too much wrong attempts, try again later!';
+                break;
+            case 'INVALID_LOGIN_CREDENTIALS':
+                errorMessage = 'The e-mail, password or both credentials are wrong!';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'This user was disabled, please contact the administrator user!';        
+                break;
+            default: 
+                break;
+
+        }
+        return throwError(errorMessage);
     }
 }

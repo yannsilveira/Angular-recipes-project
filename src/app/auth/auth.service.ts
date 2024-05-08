e44d220b3400d0a0b20a1998c7b7cd202bbd6b72
Yann Ap. da Silveira <56853103+yannsilveira@users.noreply.github.com>
2024-05-08 19:02:10 -0300

    Implemented some logic to handle the tabs of the application when the user is logged or not, also added an interceptor to hold the token for the requests.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { throwError } from "rxjs";
-import { catchError } from "rxjs/operators";
+import { BehaviorSubject, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
 
 export interface AuthResponseData {
     kind: string;
@@ -15,6 +16,8 @@ export interface AuthResponseData {
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+    user = new BehaviorSubject<User>(null);
+
     constructor(private http: HttpClient) {}
  
     signup(email: string, password: string) {
@@ -23,7 +26,9 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        }).pipe(catchError(this.handleError));
+        }).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
     }
 
     login(email: string, password: string) {
@@ -32,7 +37,15 @@ export class AuthService {
             email: email,
             password: password,
             returnSecureToken: true
-        }).pipe(catchError(this.handleError));
+        }).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+        }));
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+            const user = new User(email, userId, token, expirationDate);
+            this.user.next(user);
     }
 
     private handleError(errorRes: HttpErrorResponse) {
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+    private userSub: Subscription;
+    isAuthenticated = false;
 
-    constructor(private dataStorage: DataStorageService) {}
+    constructor(private dataStorage: DataStorageService, private authService: AuthService) {}
+
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !!user;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
+    }
 
     onSaveData() {
         this.dataStorage.storeRecipes();
